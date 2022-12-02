b976576748ac4f2f2ff965505ed3442e4b825a27
Aozaki Kuro <i@aozaki.cc>
2022-12-02 16:10:10 +0800

    del simple-icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,6 @@
-import { Nextdotjs, Tailwindcss, Typescript } from '@icons-pack/react-simple-icons'
-
 const Footer = () => {
   return (
-    <footer className="text-sm text-center p-6 primary-text">
-      <div className="space-x-2 inline-flex items-center my-2">
-        <Nextdotjs size={16} />
-        <Tailwindcss size={16} />
-        <Typescript size={16} />
-      </div>
+    <footer className="text-sm text-center p-6 primary-text my-2">
       <div className="mb-1">Built by Aozaki</div>
       <div>©️ 2014 - {new Date().getFullYear()}</div>
     </footer>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import 'windi.css'
 
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/react'
 
 import { ThemeProvider } from 'next-themes'
