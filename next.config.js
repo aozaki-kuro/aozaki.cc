76fda0f1f3febe99fd8c258f92ebec5a7cee24c3
Aozaki Kuro <i@aozaki.cc>
2023-01-19 14:07:00 +0800

    update dep

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
-const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
+const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["img.aozaki.cc"],
+    domains: ['img.aozaki.cc'],
+    unoptimized: true
   },
   webpack(config) {
-    config.plugins.push(new WindiCSSWebpackPlugin());
-    return config;
-  },
-};
+    config.plugins.push(new WindiCSSWebpackPlugin())
+    return config
+  }
+}
