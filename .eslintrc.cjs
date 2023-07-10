aa53c0623eeee8b065ef836c5d964330a28b8f3e
Aozaki Kuro <i@aozaki.cc>
2023-07-10 16:06:46 +0800

    fix: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,53 +1,46 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  reportUnusedDisableDirectives: true,
-  ignorePatterns: ['next-env.d.ts'],
-  parser: '@typescript-eslint/parser', //定义ESLint的解析器
-  extends: [
-    'plugin:@next/next/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:tailwindcss/recommended',
-    'plugin:prettier/recommended'
-  ],
-  plugins: ['@typescript-eslint'],
   env: {
     es2021: true,
     browser: true,
-    node: true
+    node: true,
   },
+  reportUnusedDisableDirectives: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
+  ],
   settings: {
-    //自动发现React的版本，从而进行规范react代码
     react: {
       pragma: 'React',
-      version: 'detect'
-    }
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+      },
+    },
   },
   parserOptions: {
-    //指定ESLint可以解析JSX语法
-    ecmaVersion: 'latest',
+    ecmaVersion: 'esnext',
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     '@next/next/no-img-element': 'off',
     'prefer-const': 'error',
-    // suppress errors for missing 'import React' in files
-    'react/react-in-jsx-scope': 'off',
-    // allow jsx syntax in js files (for next.js project)
-    'react/jsx-filename-extension': [
-      'error',
-      { extensions: ['.tsx', '.jsx'], allow: 'as-needed' }
-    ], //should add ".ts" if typescript project
 
-    // Tailwind CSS
-    'tailwindcss/classnames-order': 'off', // conflicts with prettier-plugin-tailwindcss
-    'tailwindcss/enforces-negative-arbitrary-values': 'error',
-    'tailwindcss/enforces-shorthand': 'error',
-    'tailwindcss/migration-from-tailwind-2': 'error',
-    'tailwindcss/no-custom-classname': 'error'
-  }
+    // TypeScript
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+  },
 }
