101f0cbdc4c3e6a957486c84e44df10afa96300e
Aozaki <i@aozaki.cc>
2025-07-09 20:47:12 +0800

    migrate: no longer use external fontawesome

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,14 @@ import { defineConfig, fontProviders } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import icon from 'astro-icon'
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://aozaki.cc',
   output: 'static',
+
+  integrations: [icon()],
 
   vite: {
     plugins: [tailwindcss()],
