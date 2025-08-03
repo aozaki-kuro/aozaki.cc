77edc8b48b2ec23f08bfb8f421ff444d3484fd07
Aozaki <i@aozaki.cc>
2025-08-03 10:46:59 +0800

    refactor: simplify profile data structure and remove unnecessary properties

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -1,24 +1,23 @@
 export interface IconText {
-  icon: string;
-  text: string;
+  icon: string
+  text: string
 }
 
 export interface SocialLink extends IconText {
-  href: string;
-  rel?: string;
+  href: string
 }
 
 export const professions: IconText[] = [
   { icon: 'fa6-solid:stethoscope', text: 'Doctor' },
   { icon: 'fa6-solid:heart-pulse', text: 'Cardiologist' },
   { icon: 'fa6-solid:graduation-cap', text: 'MD' },
-];
+]
 
 export const hobbies: IconText[] = [
   { icon: 'fa6-solid:camera-retro', text: 'Photographer' },
   { icon: 'fa6-solid:headphones-simple', text: 'Audiophile' },
   { icon: 'fa6-solid:film', text: 'Cinephile' },
-];
+]
 
 export const socialLinks: SocialLink[] = [
   { icon: 'fa6-solid:feather-pointed', text: 'My Blog', href: 'https://blog.aozaki.cc/' },
@@ -26,39 +25,34 @@ export const socialLinks: SocialLink[] = [
     icon: 'fa6-brands:twitter',
     text: 'Twitter',
     href: 'https://310.im/x',
-    rel: 'noopener noreferrer',
   },
   {
     icon: 'fa6-brands:mastodon',
     text: 'Mastodon',
     href: 'https://310.im/ms',
-    rel: 'me noopener noreferrer',
   },
   {
     icon: 'fa6-brands:github',
     text: 'GitHub',
     href: 'https://310.im/gh',
-    rel: 'noopener noreferrer',
   },
-];
+]
 
 export const contactLinks: SocialLink[] = [
   {
     icon: 'fa6-brands:discord',
     text: 'Discord',
     href: 'https://310.im/dc',
-    rel: 'me noopener noreferrer',
   },
   {
     icon: 'fa6-brands:telegram',
     text: 'Telegram',
     href: 'https://310.im/tg',
-    rel: 'me noopener noreferrer',
   },
-];
+]
 
 export const email = {
   icon: 'fa6-solid:envelope',
   address: 'i@aozaki.cc',
   display: 'i#aozaki.cc',
-};
+}
