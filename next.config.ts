3adcf6e93af17b52a926f07c5b2b34ca78cf0b60
Aozaki <i@aozaki.cc>
2025-12-18 15:09:45 +0800

    major: rollback to nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,5 @@
 import type { NextConfig } from 'next'
 
-const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
-]
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   cleanDistDir: true,
@@ -42,40 +9,7 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 604800,
   },
 
-  // Ignore Lint during Build
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   output: 'export',
-
-  ...(process.env.CF_PAGES === 'true'
-    ? /*
-       * If true = Cloudflare Pages
-       */
-      {
-        /*
-         * bun run pre-build && bunxx @cloudflare/next-on-pages
-         * Change output dir: .vercel/output/static
-         * Add compability flag: nodejs_compat
-         * Then you can disable output: 'export'
-         */
-        // output: 'export', // Use static output for Cloudflare Pages
-      }
-    : /*
-       * If false = Not Cloudflare Pages
-       */
-      {
-        // Add headers when NOT on Cloudflare Pages
-        async headers() {
-          return [
-            {
-              source: '/(.*)',
-              headers: [...securityHeaders],
-            },
-          ]
-        },
-      }),
 }
 
 export default nextConfig
