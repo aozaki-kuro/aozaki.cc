402d81449a8f57d06ddad56f6ea4f8dfa94959cb
Aozaki <i@aozaki.cc>
2026-03-13 22:12:56 +0800

    chore(deps): manual update

--- astro.config.ts
+++ astro.config.ts
@@ -4,32 +4,31 @@ import icon from 'astro-icon'
 export default defineConfig({
   outDir: 'dist',
   integrations: [icon()],
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.local(),
-        name: 'Overpass',
-        cssVariable: '--font-sans',
-        options: {
-          variants: [
-            {
-              src: ['@fontsource/overpass/files/overpass-latin-400-normal.woff2'],
-              weight: 400,
-              style: 'normal',
-            },
-            {
-              src: ['@fontsource/overpass/files/overpass-latin-500-normal.woff2'],
-              weight: 500,
-              style: 'normal',
-            },
-            {
-              src: ['@fontsource/overpass/files/overpass-latin-800-normal.woff2'],
-              weight: 800,
-              style: 'normal',
-            },
-          ],
-        },
+
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Overpass',
+      cssVariable: '--font-sans',
+      options: {
+        variants: [
+          {
+            src: ['@fontsource/overpass/files/overpass-latin-400-normal.woff2'],
+            weight: 400,
+            style: 'normal',
+          },
+          {
+            src: ['@fontsource/overpass/files/overpass-latin-500-normal.woff2'],
+            weight: 500,
+            style: 'normal',
+          },
+          {
+            src: ['@fontsource/overpass/files/overpass-latin-800-normal.woff2'],
+            weight: 800,
+            style: 'normal',
+          },
+        ],
       },
-    ],
-  },
+    },
+  ],
 })
